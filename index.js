ba80e2c705df78d2df2990f91de40176e53359b6
Qize <louisliu1206@yahoo.com>
2020-06-13 22:16:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function happyHolidays() { return 'Happy holidays!' }
 
 function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
+
+function happyCustomHolidayTo(holiday,name) {return `Happy ${holiday}, ${name}!`}
+
+function holidayCountdown(days,holiday) {return `It's ${days} days until ${holiday}!`}
