152380fd3bc66415f8e335bb83d8fe531b163bd9
Qize <louisliu1206@yahoo.com>
2020-06-13 21:57:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function happyHolidays() { return 'Happy holidays!' }
 
-function happyHolidaysTo(name) {console.log(`Happy holidays, ${name}!`)}
+function happyHolidaysTo(name) {return `Happy holidays, ${name}!`}
